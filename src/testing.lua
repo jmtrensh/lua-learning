e26b7a7299eacbeb8b4b20a9ce6d4bcb52158fa2
Joe Trenshaw <joe@joetrenshaw.com>
2026-02-02 20:49:38 -0500

    basic printing and functions

---
+++ src/testing.lua
@@ -0,0 +1,18 @@
+-- Basic Hello World
+
+-- [[
+-- Multi-line comment
+-- ]]
+
+print("Hello World!")
+
+local test_var = 5
+
+print("Hello ", test_var)
+
+local function test(number)
+	testing = number + 5
+	return testing
+end
+
+print(test(15))
